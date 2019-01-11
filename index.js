a95486fe81bd6cd68d61ae7ccb2842b1a13bbd69
oscar <oscar@o2webdev.com>
2019-01-11 14:09:50 -0500

    Added some comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Removes all spaces from a string
 module.exports = function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
